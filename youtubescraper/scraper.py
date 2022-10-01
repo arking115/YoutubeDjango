c57c964491bbca1879b1fdf0aaec25fefb6e6dfc
Arking <rusalexandru115@gmail.com>
2022-10-01 19:05:12 +0300

    changed f scraper into class

--- youtubescraper/scraper.py
+++ youtubescraper/scraper.py
@@ -1,24 +1,57 @@
 from unicodedata import digit
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import re
 
-def scraper(Yt_link):
-    driver = webdriver.Chrome(service=Service("/usr/local/bin/chromedriver"))
-    driver.get(Yt_link)
-    try:
-        views = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[@class='view-count style-scope ytd-video-view-count-renderer']")))
-    except:
-        views = -1
-    try:
-        name = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//h1[@class='title style-scope ytd-video-primary-info-renderer']")))
-    except:
-        name = None
-    integer_views = re.findall(r'\b\d+\b', views.text)
-    final_views= ''.join(integer_views)
-    final_name = name.text
-    driver.close()
-    return final_views,final_name
+class scraper:
+    def __init__(self, yt_link):
+        self.driver = webdriver.Chrome()
+        self.yt_link = yt_link
+        self.url_info=[]
+
+    def get_name(self, yt_link):
+        self.driver.get(yt_link)
+        try:
+            name = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//h1[@class='title style-scope ytd-video-primary-info-renderer']")))
+        except:
+            name = None
+        name = name.text
+        return name
+
+    def get_views(self):
+        try:
+            views = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[@class='view-count style-scope ytd-video-view-count-renderer']")))
+        except:
+            views = -1
+        integer_views = re.findall(r'\b\d+\b', views.text)
+        final_views= ''.join(integer_views)
+        final_views = int(final_views)
+        return final_views
+
+    #def get_info(self, yt_url):
+      #  self.driver.get(yt_url)
+       # try:
+       #     views = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[@class='view-count style-scope ytd-video-view-count-renderer']")))
+       # except:
+       #     views = -1
+       # try:
+       #     name = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//h1[@class='title style-scope ytd-video-primary-info-renderer']")))
+       # except:
+       #     name = None
+       # views = views.text
+       # name = name.text
+       # return views, name
+
+    def extract_url_info(self):
+        self.url_info.append(self.get_name(self.yt_link))
+        self.url_info.append(self.get_views())
+        self.driver.close()
+    
+    def run(self):
+        self.extract_url_info()
+        print(self.url_info)
+
+yt = scraper('https://www.youtube.com/watch?v=mcp-6mAnOnA')
+yt.run()
